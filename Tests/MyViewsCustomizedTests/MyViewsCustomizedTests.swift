e739414027ba0bd39d5d16acdb9cbc93e787254b
niv ben-porath <nbpapps@gmail.com>
2020-02-14 14:13:46 +0200

    added Unit tests for label and button

--- Tests/MyViewsCustomizedTests/MyViewsCustomizedTests.swift
+++ Tests/MyViewsCustomizedTests/MyViewsCustomizedTests.swift
@@ -2,14 +2,27 @@ import XCTest
 @testable import MyViewsCustomized
 
 final class MyViewsCustomizedTests: XCTestCase {
-    func testExample() {
+    func testLabel() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-//        XCTAssertEqual(MyViewsCustomized().text, "Hello, World!")
+        let myLabel = NBPLabel(textAlignment: .center, fontSize: 16.0, weight: .bold, color: .red)
+        
+        XCTAssertEqual(myLabel.textColor,  UIColor.red, "label color should be red")
+        XCTAssertEqual(myLabel.textAlignment, NSTextAlignment.center, "label should have a center text alignment")
+        XCTAssertEqual(myLabel.font, UIFont.systemFont(ofSize: 16.0, weight: .bold), "label should have font of size 16 and blod weight")
+        
+    }
+    
+    func testButton() {
+        let myButton = NBPButton(backgroundColor: .red, title: "My button")
+            
+        XCTAssertEqual(myButton.backgroundColor, UIColor.red, "button BG color should be red")
+        XCTAssertEqual(myButton.titleLabel?.text, "My button", "button title should be 'My button'")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testLabel", testLabel),
+        ("testButton",testButton)
     ]
 }
